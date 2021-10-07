f13718a21f73d342f36f217d3416585439ab6761
Oleg Mikhaylov <cherbot@mail.ru>
2021-10-07 22:42:15 +0300

    button

--- www/js/script.js
+++ www/js/script.js
@@ -1,8 +1,18 @@
 $(function () {
-    $("#toggleButton").click(function () {
+    $("#downButton").click(function () {
         console.log(123);
-        $("#more").remove();
-        $(".exmp-point-hidden").slideToggle();
+        $("#more").fadeOut();
+        $(".exmp-point-hidden").slideDown();
+        $("#clear").slideDown()
+        //$('#toggleSection').slideToggle();
+    });
+});
+$(function () {
+    $("#upButton").click(function () {
+        console.log(124);
+        $("#clear").fadeOut();
+        $(".exmp-point-hidden").slideUp();
+        $("#more").fadeIn();
         //$('#toggleSection').slideToggle();
     });
 });
